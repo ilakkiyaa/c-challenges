1298b39846d60a121e71211a86ed35ac735ef4e5
ilakkiyaa <ilakkiyapmr96@gmail.com>
2017-05-16 23:09:26 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
-int main(){
-  int i,f=1,num;
- 
-  printf("Enter a number: ");
-  scanf("%d",&num);
- 
-  for(i=1;i<=num;i++)
-      f=f*i;
- 
-  printf("Factorial of %d is: %d",num,f);
-  return 0;
+int main()
+{
+int i,f=1,num;
+printf("Enter a number: ");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+f=f*i;
+printf("Factorial of %d is: %d",num,f);
 }

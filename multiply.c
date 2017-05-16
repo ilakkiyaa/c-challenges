3017335e2be2f33f815229bbcde7c6b9c090e237
ilakkiyaa <ilakkiyapmr96@gmail.com>
2017-05-16 23:15:30 +0530

    Update multiply.c

--- multiply.c
+++ multiply.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
- {
- int n, term, i;
-    printf("Enter a number to print multiplication table\n");
-    scanf("%d",&n);
-    printf("Enter number of terms in table\n");
-    scanf("%d",&term);
-    for(i = 1; i <= term; ++i){
-        printf("%d X %d = %d\n", n, i, n*i);
-    }
+{
+int n, term, i;
+printf("Enter a number to print multiplication table\n");
+scanf("%d",&n);
+printf("Enter number of terms in table\n");
+scanf("%d",&term);
+for(i = 1; i <= term; ++i)
+{
+printf("%d X %d = %d\n", n, i, n*i);
+}
 }

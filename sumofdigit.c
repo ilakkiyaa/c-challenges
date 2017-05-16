e101ce40187c54aef8fb7ab8886ffbde7e20832a
ilakkiyaa <ilakkiyapmr96@gmail.com>
2017-05-16 20:46:49 +0530

    Update sumofdigit.c

--- sumofdigit.c
+++ sumofdigit.c
@@ -1,27 +1,14 @@
 #include<stdio.h>
-
 int main()
-
 {
-
 int n,sum,i=0;
-
 printf("enter the integer ");
-
 scanf("%d",&n);
-
 sum=n;
-
 while(sum!=0)
-
 {
-
 sum=sum/10;
-
 ++i;
-
 }
-
 printf("number og digits in integer %d",i);
-
 }

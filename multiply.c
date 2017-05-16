f48738de27622025a88fe3b6e6a78151e2b69610
ilakkiyaa <ilakkiyapmr96@gmail.com>
2017-05-16 23:14:21 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+ int n, term, i;
+    printf("Enter a number to print multiplication table\n");
+    scanf("%d",&n);
+    printf("Enter number of terms in table\n");
+    scanf("%d",&term);
+    for(i = 1; i <= term; ++i){
+        printf("%d X %d = %d\n", n, i, n*i);
+    }
+}
